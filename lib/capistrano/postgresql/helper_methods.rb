6dc83bf324bb78c3f1564029a6fdc8ad29822752
Bruno Sutic <bruno.sutic@gmail.com>
2014-03-30 13:07:26 +0200

    Styling refactoring in template helper method

--- lib/capistrano/postgresql/helper_methods.rb
+++ lib/capistrano/postgresql/helper_methods.rb
@@ -9,7 +9,8 @@ module Capistrano
         # If there's no customized file in your rails app template directory,
         # proceed with the default.
         unless File.exists?(config_file)
-          config_file = File.join(File.dirname(__FILE__), "../../generators/capistrano/postgresql/templates/#{template_name}")
+          default_config_path = "../../generators/capistrano/postgresql/templates/#{template_name}"
+          config_file = File.join(File.dirname(__FILE__), default_config_path)
         end
         StringIO.new ERB.new(File.read(config_file)).result(binding)
       end
