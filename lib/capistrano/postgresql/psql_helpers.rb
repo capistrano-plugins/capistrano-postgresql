36c2816eb9cdf0f8d931ef5e1f9fbe034b894b41
Nathan Pierce <connarpierce@gmail.com>
2017-12-18 18:20:37 -0500

    issues/44: Fixed

--- lib/capistrano/postgresql/psql_helpers.rb
+++ lib/capistrano/postgresql/psql_helpers.rb
@@ -17,11 +17,11 @@ module Capistrano
       end
 
       def db_user_exists?
-        psql_on_db fetch(:pg_database),'-tAc', %Q{"SELECT 1 FROM pg_roles WHERE rolname='#{fetch(:pg_user)}';" | grep -q 1}
+        psql_on_db fetch(:pg_system_db),'-tAc', %Q{"SELECT 1 FROM pg_roles WHERE rolname='#{fetch(:pg_user)}';" | grep -q 1}
       end
 
       def database_exists?
-        psql_on_db fetch(:pg_database), '-tAc', %Q{"SELECT 1 FROM pg_database WHERE datname='#{fetch(:pg_database)}';" | grep -q 1}
+        psql_on_db fetch(:pg_system_db), '-tAc', %Q{"SELECT 1 FROM pg_database WHERE datname='#{fetch(:pg_database)}';" | grep -q 1}
       end
 
       private
